a23c72b43b1bb12953836dc8679aab300d4995e4
alonaly24-meet <alona.lynfield@gmail.com>
2023-07-23 16:30:21 +0300

    Update app.py

--- authentication-lab/app.py
+++ authentication-lab/app.py
@@ -6,6 +6,18 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'super-secret-key'
 
 
+config = { "apiKey": "AIzaSyCPENPTNkxNxTVBwVr-bJ9UfhNGFsr7Wwg",
+  "authDomain": "bibi-49330.firebaseapp.com",
+  "projectId": "bibi-49330",
+  "storageBucket": "bibi-49330.appspot.com",
+  "messagingSenderId": "582709654391",
+  'appId': "1:582709654391:web:c2b2d562c00cc5afede772",
+  "measurementId": "G-YKRKPG041V", "databaseURL":""}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+
 @app.route('/', methods=['GET', 'POST'])
 def signin():
     return render_template("signin.html")
@@ -19,6 +31,8 @@ def signup():
 @app.route('/add_tweet', methods=['GET', 'POST'])
 def add_tweet():
     return render_template("add_tweet.html")
+
+
 
 
 if __name__ == '__main__':
